fa9c1d3eee7e19d7fa23dd6d143bf2a1a9df7167
David Kalmus <dejvios@seznam.cz>
2026-01-03 00:25:19 +0100

    app

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ const Home = () => {
     <Navbar />
     <main>
       <section id='home' className='h-screen' style={{backgroundImage: 'url("/background-1.png")'}}></section>
-       <section id='about' className='h-screen'></section>
+       <section id='about' className='h-screen bg-slate-900/30'></section>
        <section id='skills' className='h-screen'></section>
         <section id='projects' className='h-screen'></section>
         <section id='contact' className='h-screen'></section>
