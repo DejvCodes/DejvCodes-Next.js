31020844836b32652453bb1a705ccacfbf5ce914
David Kalmus <dejvios@seznam.cz>
2026-01-02 22:29:39 +0100

    constants.ts

---
+++ constants.ts
@@ -0,0 +1,9 @@
+import {NavLink} from "./types";
+
+export const NAV_LINKS: NavLink[] = [
+  { id: 1, path: "#home", text: "Home" },
+  { id: 2, path: "#about", text: "About" },
+  { id: 3, path: "#skills", text: "Skills" },
+  { id: 4, path: "#projects", text: "Projects" },
+  { id: 5, path: "#contact", text: "Contact" },
+];
