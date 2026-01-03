6b2d0fffd26ae52b9b6f62ae7535f7efd39a5d6c
David Kalmus <dejvios@seznam.cz>
2026-01-03 10:27:35 +0100

    app

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,18 @@
 'use client';
-
 import Navbar from '@/components/Navbar';
 
 const Home = () => {
 
-  return <div>
+  return <>
     <Navbar />
     <main>
-      <section id='home' className='h-screen' style={{backgroundImage: 'url("/background-1.png")'}}></section>
-       <section id='about' className='h-screen bg-slate-900/30'></section>
-       <section id='skills' className='h-screen'></section>
-        <section id='projects' className='h-screen'></section>
-        <section id='contact' className='h-screen'></section>
+      <section id='home' className='h-screen bg-linear-to-b from-black via-slate-960 to-slate-950'></section>
+      <section id='about' className='h-screen bg-slate-900/30'></section> 
+      <section id='skills' className='h-screen bg-slate-900/30'></section>
+      <section id='projects' className='h-screen bg-slate-900/30'></section>
+      <section id='contact' className='h-screen bg-slate-900/30'></section>
     </main>
-  </div>
+  </>
 }
 
 export default Home
