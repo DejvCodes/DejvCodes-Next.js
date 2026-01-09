b83a11f300af8c75edf0e1652d464dc039aab681
David Kalmus <dejvios@seznam.cz>
2026-01-09 18:49:08 +0100

    Skills

---
+++ components/Skills.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Skills: React.FC = () => {
+  return <section id='skills' className='bg-slate-900/20 py-20'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8'>
+        Skills
+    </div>
+  </section>
+}
+
+export default Skills
