73089f4f0fcccbfa2f408bef512da2b9f9db4731
David Kalmus <dejvios@seznam.cz>
2026-01-09 18:48:52 +0100

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Hero from '@/components/Hero';
 import About from '@/components/About';
+import Skills from '@/components/Skills';
 import Navbar from '@/components/Navbar';
 import StarfieldBG from '@/components/StarfieldBG';
 
@@ -11,7 +12,7 @@ const Home = () => {
       <Hero />
       <StarfieldBG />
       <About />
-      <section id='skills' className='h-screen bg-slate-900/30'></section>
+      <Skills />
       <section id='projects' className='h-screen bg-slate-900/30'></section>
       <section id='contact' className='h-screen bg-slate-900/30'></section>
     </main>
