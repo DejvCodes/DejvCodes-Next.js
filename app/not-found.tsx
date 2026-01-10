aaec103fedd4f18721deaadd3b176a783e263fa7
David Kalmus <dejvios@seznam.cz>
2026-01-10 17:57:20 +0100

    not-found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 const NotFound = () => {
   return (
-    <div className='min-h-screen flex items-center justify-center bg-gradient-to-b from-black to-slate-900'>
+    <div className='min-h-screen flex items-center justify-center bg-linear-to-b from-black to-slate-900'>
       <div className='text-center px-4'>
         <h1 className='text-9xl font-bold text-white mb-4'>404</h1>
         <h2 className='text-3xl md:text-4xl font-semibold text-gray-300 mb-6'>
