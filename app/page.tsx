e0eaa42765acdb8b536c2d5eda136071bbf65735
David Kalmus <dejvios@seznam.cz>
2026-01-11 22:30:59 +0100

    app

--- app/page.tsx
+++ app/page.tsx
@@ -3,19 +3,22 @@ import Hero from '@/components/Hero';
 import About from '@/components/About';
 import Skills from '@/components/Skills';
 import Navbar from '@/components/Navbar';
-import StarfieldBG from '@/components/StarfieldBG';
+import Contact from '@/components/Contact';
 import Projects from '@/components/Projects';
+import StarfieldBG from '@/components/StarfieldBG';
+import SocialNetworks from '@/components/SocialNetworks';
 
 const Home = () => {
   return <>
     <Navbar />
+    <SocialNetworks />
     <main>
       <Hero />
       <StarfieldBG />
       <About />
       <Skills />
       <Projects />
-      <section id='contact' className='h-screen bg-slate-900/30'></section>
+      <Contact />
     </main>
   </>
 }
